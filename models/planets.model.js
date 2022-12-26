96a69561e973b4ffe326c30a181614414786c72c
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-12-26 14:08:06 +0300

    separate models

---
+++ models/planets.model.js
@@ -0,0 +1,14 @@
+const planets = [
+    {
+        id: 0,
+        planet_name: 'Keppler-442b',
+        host_name: 'Keppler-442'
+    },
+    {
+        id: 1,
+        planet_name: 'Centauri b',
+        host_name: 'alpha-centauri'
+    }
+]
+
+module.exports = {planets}
