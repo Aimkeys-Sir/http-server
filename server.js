bdedb592ef74e0a2b86936c39e7eeb4ef86e44e8
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-12-26 10:00:23 +0300

    add middleware

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express')
+
+const app = express()
+
+const PORT = 3000
+
+const planets = [
+    {
+        id: 0,
+        planet_name: 'Keppler-442b',
+        host_name: 'Keppler-442'
+    },
+    {
+        id: 1,
+        planet_name: 'Centauri b',
+        host_name: 'alpha-centauri'
+    }
+]
+
+app.listen(PORT, ()=>{
+    console.log(`Server Listening at ${PORT}`);
+})
+
+
+app.use((req,res,next)=>{
+    const start = Date.now()
+    console.log(`${req.method} ${req.url} ${new Date()}`)
+    next()
+    const timeTaken= Date.now() - start 
+
+    console.log(`\r ${timeTaken}ms`)
+})
+
+app.get('/',(req,res)=>{
+    res.send("Here we are!")
+})
+
+app.get("/messages", (req,res)=>{
+    res.send('<h3>To infinity and beyond</h3>')
+})
+
+app.get("/places", (req,res)=>{
+    res.json(planets)
+})
+
+app.get('/places/:id',(req,res)=>{
+    const placeId = +req.params.id
+    const planet = planets[placeId]
+    if(planet){
+        res.json(planet)
+    }else{
+        res.status(404).json({
+            error: "planet does not exist!"
+        })
+    }
+})
