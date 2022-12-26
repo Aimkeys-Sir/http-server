9b6d3c5b1516d860044878727a30cffa52fabd6e
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-12-26 10:23:37 +0300

    post data

--- server.js
+++ server.js
@@ -17,40 +17,58 @@ const planets = [
     }
 ]
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server Listening at ${PORT}`);
 })
 
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     const start = Date.now()
     console.log(`${req.method} ${req.url} ${new Date()}`)
     next()
-    const timeTaken= Date.now() - start 
+    const timeTaken = Date.now() - start
 
     console.log(`\r ${timeTaken}ms`)
 })
 
-app.get('/',(req,res)=>{
+app.use(express.json())
+
+app.get('/', (req, res) => {
     res.send("Here we are!")
 })
 
-app.get("/messages", (req,res)=>{
+app.get("/messages", (req, res) => {
     res.send('<h3>To infinity and beyond</h3>')
 })
 
-app.get("/places", (req,res)=>{
+app.get("/places", (req, res) => {
     res.json(planets)
 })
 
-app.get('/places/:id',(req,res)=>{
+app.get('/places/:id', (req, res) => {
     const placeId = +req.params.id
     const planet = planets[placeId]
-    if(planet){
+    if (planet) {
         res.json(planet)
-    }else{
+    } else {
         res.status(404).json({
             error: "planet does not exist!"
         })
     }
+})
+
+app.post('/places', (req, res) => {
+    if (!req.body.planet_name) {
+        res.status(422)
+            .json({ error: 'Planet name must exist' })
+    } else {
+        const newPlanet = {
+            id: planets.length,
+            ...req.body
+        }
+        planets.push(newPlanet)
+
+        res.json(newPlanet)
+    }
+
 })
